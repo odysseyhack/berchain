f945597bb95a5e0dc52c722bae0ae48ec6a58193
Carlos E. Salazar <ce.salazar@gmail.com>
2019-04-14 01:03:43 +0200

    Add data from db in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,29 +20,8 @@ class DashboardController extends BaseController
         $dbProject = Project::find($id);
         $project = $projectTransformer->transform($dbProject);
 
-        //dd($filteredProjects, $project);
-
-        $db =[
-            [
-                'company_name' => 'FOO',
-                'description' => 'Description of FOO',
-                'some_other_field' => 'This is just another field'
-            ],
-            [
-                'company_name' => 'BAR',
-                'description' => 'Description of BAR',
-                'some_other_field' => 'This is just another field'
-            ]
-        ];
-
-        $data = $db[$id];
-
-        // TODO Get the badge data from the store
         return view('dashboard')
             ->with([
-                'company_name' => $data['company_name'],
-                'description' => $data['description'],
-                'some_other_field' => $data['some_other_field'],
                 'project' => $project
             ]);
     }
