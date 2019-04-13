9ebea30087ad2f1ca18129b825b6f546f3d5a7ed
Artur Ch <chmarus@gmail.com>
2019-04-14 01:18:36 +0200

    Store kpis at contract

--- resources/js/components/CreateTransaction.js
+++ resources/js/components/CreateTransaction.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import contract from 'truffle-contract'
+import BadgeDef from '../contracts/Badge.json'
+import Web3 from 'web3'
 
 export default class CreateTransaction extends Component {
   constructor (props) {
@@ -8,7 +11,8 @@ export default class CreateTransaction extends Component {
       amount: '',
       donatorId: '',
       donators: [],
-      errors: []
+      errors: [],
+      status: 'idle'
     }
     this.handleFieldChange = this.handleFieldChange.bind(this)
     this.hasErrorFor = this.hasErrorFor.bind(this)
@@ -33,8 +37,32 @@ export default class CreateTransaction extends Component {
     })
   }
 
-  createTransaction (event) {
+  async getAccount () {
+    const web3 = new Web3(window.ethereum)
+    const accounts = await web3.eth.getAccounts()
+
+    return accounts[0]
+  }
+
+  getBadgeContract () {
+    const Badge = contract(BadgeDef)
+    Badge.setProvider(window.ethereum)
+
+    return Badge
+  }
+
+  async createTransactionAtBlockchain (data) {
+    const Badge = this.getBadgeContract()
+    const instance = await Badge.at('0x4A44e1eb04A7689F96facf61119042e06D555e69')
+    const address = await this.getAccount()
+    const transactionAddr = await instance.addTransaction(data, {from: address})
+
+    return transactionAddr.tx
+  }
+
+  async createTransaction (event) {
     event.preventDefault()
+    this.setState({status: 'working'})
 
     //const { history } = this.props
 
@@ -44,11 +72,14 @@ export default class CreateTransaction extends Component {
     }
 
     axios.post('/api/projects/1/donation', donation)
-      .then(response => {
+      .then( async (response) => {
         // redirect to the homepage
         //history.push('/')
         console.log(response);
-        //Artur
+        const data = `jobsPerHectare: ${response.data.kpis.jobsPerHectare}, tonsOfBiomass: ${response.data.kpis.tonsOfBiomass}, reductionOfCo2: ${response.data.kpis.reductionOfCo2}`
+        const transactionAddr = await this.createTransactionAtBlockchain(data)
+        console.log(transactionAddr)
+        this.setState({status: 'completed'})
       })
       .catch(error => {
         this.setState({
@@ -73,13 +104,14 @@ export default class CreateTransaction extends Component {
   getDonators() {
 
     console.log('get donators', this.state.donators)
-    return <select className="form-control">
+    return <select name='donatorId' className="form-control" onChange={this.handleFieldChange}>
       {this.state.donators.map(donator => {
         return <option value={donator.id}>{donator.name}</option>
       })}
     </select>
   }
   render () {
+    const { status } = this.state
 
     return (
       <div className='container'>
@@ -107,7 +139,11 @@ export default class CreateTransaction extends Component {
                 {this.renderErrorFor('donator')}
               </div>
 
-              <button className='btn btn-primary'>Create</button>
+              <button className='btn btn-primary'>
+                {status === 'idle' && 'Create'}
+                {status === 'working' && 'Please wait...'}
+                {status === 'completed' && 'Done!'}
+              </button>
             </form>
           </div>
         </div>
