2cbd6c1b1a201c1ff5b0c66511508447e0d58b23
Carlos E. Salazar <ce.salazar@gmail.com>
2019-04-13 13:47:33 +0200

    Add routes for the badge and corporate-dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+// Redirect to home if no badge ID is specified
+Route::get('/badge', function () {
+    return redirect()->route('home');
 });
+
+// Show the badge
+Route::get('/badge/{id}', 'BadgeController@showBadge');
+
+// Redirect to home if no dashboard ID is specified
+Route::get('/dashboard', function () {
+    return redirect()->route('home');
+});
+
+// Show the corporate-dashboard (public)
+Route::get('/dashboard/{id}', 'DashboardController@showDashboard');
