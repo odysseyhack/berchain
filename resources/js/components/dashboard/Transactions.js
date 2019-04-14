fe7cf0687de0e33ed0361f9a8c79ab47741411da
Carlos E. Salazar <ce.salazar@gmail.com>
2019-04-14 10:09:50 +0200

    Refactor components

---
+++ resources/js/components/dashboard/Transactions.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const transactions = (props) => {
+  return (
+    <Fragment>
+      These are the transactions
+    </Fragment>
+  )
+}
+
+export default transactions
